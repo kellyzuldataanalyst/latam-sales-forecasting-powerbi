43e64613828d292a3af5185b66bb38636511f233
kellyzuldataanalyst <kellyzuluagadataset@gmail.com>
2026-04-30 20:01:23 -0500

    Update queries.sql
    Consultas de SQL comentadas

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,9 @@
+-- En esta consulta se ven las por país
+SELECT country, SUM(units_sold * price) AS revenue
+FROM sales
+GROUP BY country;
 
+-- Ventas por categoría
+SELECT category, SUM(units_sold) AS total_units
+FROM sales
+GROUP BY category;
